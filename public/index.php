fcf2aea66247894aa65355d873c5fee8eb77149c
Pharos1 <pharosisdoingnothing@gmail.com>
2025-10-07 09:52:56 +0300

    Test Page

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/styles.css"/>
+    </head>
+    <body>
+        <h1> Supplement Shop </h1>
+        <hr>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio debitis dolores magnam modi porro accusamus commodi reiciendis quas? Nam eveniet sint obcaecati accusamus tempora odio numquam reiciendis quas eaque consequuntur. </p>
+        
+        <form method="post">
+            <input type="submit" name="test" value="A Button!">
+        </form>
+    </body>
+</html>
+
 <?php
-    echo "Working ig";
-    echo "Test of branch 2";
+    if (array_key_exists('test', $_POST)){
+        echo "Cliked!<br>";
+    }
 ?>
